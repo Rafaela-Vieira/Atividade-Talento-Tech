65fd7ff891a90e29b2c5b976fbc188c83a593e6e
Rafaela Vieira <fernandes.rafaelavieira@gmail.com>
2024-12-06 13:42:57 -0300

    adicionar arquivo de curiosidades.tecnologias

---
+++ curiosidades.tecnologias.js
@@ -0,0 +1,6 @@
+//O mascote do Mozilla Firefox não é uma raposa.
+//A origem da palavra "robô" remete a trabalho forçado.
+//Empresas de tecnologia costumam testar produtos na Nova Zelândia.
+//O celular no bolso e a síndrome da vibração fantasma.
+//A primeira câmera do mundo levou oito horas para tirar uma foto.
+//O e-mail foi criado antes da internet.
